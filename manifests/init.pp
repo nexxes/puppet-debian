23a991eed4c6e7e1d343763fbc38e500c031a62c
Dennis Birkholz <dennis@birkholz.biz>
2016-02-13 01:53:08 +0100

    Install joe and disable wordwrap

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,4 +2,12 @@ class nexxes_debian {
 	patch::file { '/etc/inputrc':
 		diff_source => 'puppet:///modules/nexxes_debian/inputrc.patch'
 	}
+	
+	package { 'joe':
+		ensure => 'installed',
+	}
+	
+	patch::file { '/etc/joe/ftyperc':
+		diff_source => 'puppet:///modules/nexxes_debian/joe/ftyperc.patch',
+	}
 }
