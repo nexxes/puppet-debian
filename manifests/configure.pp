0bf33636519ef8fab975d7221e342de5af1ca703
Dennis Birkholz <birkholz@pubgrade.com>
2016-02-26 12:35:58 +0100

    Make compatible with old puppet version 2.7

--- manifests/configure.pp
+++ manifests/configure.pp
@@ -1,44 +1,44 @@
 class makedebianusable::configure {
 	patch::file { '/etc/inputrc':
-		diff_source => "puppet:///modules/${module_name}/inputrc.patch",
+		diff_source => "puppet://$server/files/${module_name}/inputrc.patch",
 	}
 	
 	# Joe
 	patch::file { '/etc/joe/ftyperc':
-		diff_source => "puppet:///modules/${module_name}/joe/ftyperc.patch",
+		diff_source => "puppet://$server/files/${module_name}/joe/ftyperc.patch",
 	}
 	
 	# Screen
 	patch::file { '/etc/screenrc':
-		diff_source => "puppet:///modules/${module_name}/screenrc.patch",
+		diff_source => "puppet://$server/files/${module_name}/screenrc.patch",
 	}
 	
 	# Bash
 	patch::file { '/etc/bash.bashrc':
-		diff_source => "puppet:///modules/${module_name}/bash.bashrc.patch",
+		diff_source => "puppet://$server/files/${module_name}/bash.bashrc.patch",
 	}
 	
 	file { '/etc/profile.d/aliases.bash':
 		ensure => 'present',
-		source => "puppet:///modules/${module_name}/profile.d/aliases.bash",
+		source => "puppet://$server/files/${module_name}/profile.d/aliases.bash",
 	}
 	
 	file { '/etc/profile.d/history.bash':
 		ensure => 'present',
-		source => "puppet:///modules/${module_name}/profile.d/history.bash",
+		source => "puppet://$server/files/${module_name}/profile.d/history.bash",
 	}
 	
 	file { '/etc/profile.d/prompt.bash':
 		ensure => 'present',
-		source => "puppet:///modules/${module_name}/profile.d/prompt.bash",
+		source => "puppet://$server/files/${module_name}/profile.d/prompt.bash",
 	}
 	
 	# SSH
 	patch::file { '/etc/ssh/ssh_config':
-		diff_source => "puppet:///modules/${module_name}/ssh/ssh_config.patch",
+		diff_source => "puppet://$server/files/${module_name}/ssh/ssh_config.patch",
 	}
 	
 	patch::file { '/etc/ssh/sshd_config':
-		diff_source => "puppet:///modules/${module_name}/ssh/sshd_config.patch",
+		diff_source => "puppet://$server/files/${module_name}/ssh/sshd_config.patch",
 	}
 }
--- manifests/prepare.pp
+++ manifests/prepare.pp
@@ -1,7 +1,7 @@
 class makedebianusable::prepare {
 	class { 'apt':
-		update => { 'frequency'    => 'daily', },
-		purge  => { 'sources.list' => true },
+		purge_sources_list   => true,
+		apt_update_frequency => 'daily',
 	}
 	
 	apt::source { 'debian_01_main':
@@ -9,7 +9,8 @@ class makedebianusable::prepare {
 		location => 'http://ftp.de.debian.org/debian/',
 		release  => "${lsbdistcodename}",
 		repos    => 'main non-free contrib',
-		include  => { 'src' => true, 'deb' => true, },
+		include_src => true,
+		include_deb => true,
 	}
 	
 	apt::source { 'debian_02_main_mirror':
@@ -17,7 +18,8 @@ class makedebianusable::prepare {
 		location => 'http://ftp2.de.debian.org/debian/',
 		release  => "${lsbdistcodename}",
 		repos    => 'main non-free contrib',
-		include  => { 'src' => true, 'deb' => true, },
+		include_src => true,
+		include_deb => true,
 	}
 	
 	apt::source { 'debian_03_security_mirror':
@@ -25,7 +27,8 @@ class makedebianusable::prepare {
 		location => 'http://ftp.hosteurope.de/mirror/ftp.debian.org/debian-security/',
 		release  => "${lsbdistcodename}/updates",
 		repos    => 'main contrib non-free',
-		include  => { 'src' => true, 'deb' => true, },
+		include_src => true,
+		include_deb => true,
 	}
 	
 	apt::source { 'debian_04_security':
@@ -33,7 +36,8 @@ class makedebianusable::prepare {
 		location => 'http://security.debian.org/',
 		release  => "${lsbdistcodename}/updates",
 		repos    => 'main contrib non-free',
-		include  => { 'src' => true, 'deb' => true, },
+		include_src => true,
+		include_deb => true,
 	}
 	
 	apt::source { 'debian_05_updates':
@@ -41,7 +45,8 @@ class makedebianusable::prepare {
 		location => 'http://ftp.de.debian.org/debian/',
 		release  => "${lsbdistcodename}-updates",
 		repos    => 'main contrib non-free',
-		include  => { 'src' => true, 'deb' => true, },
+		include_src => true,
+		include_deb => true,
 	}
 	
 	apt::source { 'debian_06_updates_mirror':
@@ -49,6 +54,7 @@ class makedebianusable::prepare {
 		location => 'http://ftp2.de.debian.org/debian/',
 		release  => "${lsbdistcodename}-updates",
 		repos    => 'main contrib non-free',
-		include  => { 'src' => true, 'deb' => true, },
+		include_src => true,
+		include_deb => true,
 	}
 }
