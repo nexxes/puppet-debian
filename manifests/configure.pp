dacdb14ea79498b5172c69d5c8515c5b5a87f20b
Dennis Birkholz <birkholz@pubgrade.com>
2019-02-26 15:18:33 +0100

    Use resource url notation valid for newer puppet versions

--- manifests/configure.pp
+++ manifests/configure.pp
@@ -1,48 +1,48 @@
 class makedebianusable::configure {
 	patch::file { '/etc/inputrc':
-		diff_source => "puppet://$server/files/${module_name}/inputrc.patch",
+		diff_source => "puppet:///modules/${module_name}/inputrc.patch",
 	}
 	
 	# Joe
 	patch::file { '/etc/joe/ftyperc':
-		diff_source => "puppet://$server/files/${module_name}/joe/ftyperc.patch",
+		diff_source => "puppet:///modules/${module_name}/${lsbdistcodename}/joe/ftyperc.patch",
 	}
 	
 	# Screen
 	patch::file { '/etc/screenrc':
-		diff_source => "puppet://$server/files/${module_name}/screenrc.patch",
+		diff_source => "puppet:///modules/${module_name}/screenrc.patch",
 	}
 	
 	# Bash
 	patch::file { '/etc/bash.bashrc':
-		diff_source => "puppet://$server/files/${module_name}/bash.bashrc.patch",
+		diff_source => "puppet:///modules/${module_name}/bash.bashrc.patch",
 	}
 	
 	file { '/etc/profile.d/aliases.bash':
 		ensure => 'present',
-		source => "puppet://$server/files/${module_name}/profile.d/aliases.bash",
+		source => "puppet:///modules/${module_name}/profile.d/aliases.bash",
 	}
 	
 	file { '/etc/profile.d/history.bash':
 		ensure => 'present',
-		source => "puppet://$server/files/${module_name}/profile.d/history.bash",
+		source => "puppet:///modules/${module_name}/profile.d/history.bash",
 	}
 	
 	file { '/etc/profile.d/prompt.bash':
 		ensure => 'present',
-		source => "puppet://$server/files/${module_name}/profile.d/prompt.bash",
+		source => "puppet:///modules/${module_name}/profile.d/prompt.bash",
 	}
 	
 	# SSH
 	file { '/etc/ssh/ssh_config':
 		ensure => 'present',
-		source => "puppet://$server/files/${module_name}/${lsbdistcodename}/ssh/ssh_config",
+		source => "puppet:///modules/${module_name}/${lsbdistcodename}/ssh/ssh_config",
 		notify => Service["ssh"],
 	}
 	
 	file { '/etc/ssh/sshd_config':
 		ensure => 'present',
-		source => "puppet://$server/files/${module_name}/${lsbdistcodename}/ssh/sshd_config",
+		source => "puppet:///modules/${module_name}/${lsbdistcodename}/ssh/sshd_config",
 		notify => Service["ssh"],
 	}
 }
